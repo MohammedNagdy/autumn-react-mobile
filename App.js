e180a31c2465296e60ea6c9c8686889c883487b8
Mohammed Nagdy <42354467+MohammedNagdy@users.noreply.github.com>
2022-10-17 14:34:30 +0200

    Added More Navigation

--- App.js
+++ App.js
@@ -4,12 +4,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Import screens
-import {HomeScreen} from './screens/Home';
-import {SignupScreen} from './screens/Signup';
-import {SignupSellerScreen} from './screens/SignupSeller';
-import { ExploreScreen} from './screens/Explore';
+import { HomeScreen } from './screens/Home';
+import { SignupScreen } from './screens/Signup';
+import { SignupSellerScreen } from './screens/SignupSeller';
+import { ExploreScreen } from './screens/Explore';
 import { LoginScreen } from './screens/Login';
-
+import { SuccessSellerScreen } from './screens/SuccessSeller';
+import { RegisterScreen } from './screens/Register';
+import { ProductScreen } from './screens/Product';
+import { CheckoutScreen } from './screens/Checkout';
 
 const Stack = createNativeStackNavigator()
 
@@ -22,6 +25,11 @@ export default function App() {
         <Stack.Screen name="SignupSeller" component={SignupSellerScreen}/>
         <Stack.Screen name="Explore" component={ExploreScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="SuccessSeller" component={SuccessSellerScreen}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
+        <Stack.Screen name="Product" component={ProductScreen}/>
+        <Stack.Screen name="Checkout" component={CheckoutScreen}/>
+      
 
       </Stack.Navigator>
     </NavigationContainer>
---
+++ screens/Register.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { StyleSheet, Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+export const RegisterScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>This is Mobile Register page!</Text>
+            <View style={styles.buttons}>
+                <Button title="Go Back" onPress={() => navigation.goBack()}></Button>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({ 
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttons: {
+        display: "flex",
+        flexDirection: "row"
+    }
+  });
